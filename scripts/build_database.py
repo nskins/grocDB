73fed0074e0ab68ca82d95fd4b7ee9c1ac05db5c
Nicholas Skinsacos <nskins@umich.edu>
2015-11-04 18:10:52 -0500

    Adding build_database script

---
+++ scripts/build_database.py
@@ -0,0 +1,33 @@
+import os
+import sqlite3
+
+# Open groc.db in the appropriate location.
+conn = sqlite3.connect(os.getcwd() + '//src//groc.db')
+
+# Establish a cursor.
+c = conn.cursor()
+
+# Execute SQL statements.
+c.execute(
+'''CREATE TABLE recipes (
+name TEXT PRIMARY KEY,
+directions TEXT,
+serves INT);''')
+
+c.execute(
+'''CREATE TABLE madewith (
+recipe TEXT REFERENCES recipes(name),
+ingredient TEXT,
+ingredientFg TEXT,
+ingredientUnit TEXT,
+ingredientQty NUMERIC(7,2),
+PRIMARY KEY(recipe, ingredient));''')
+
+# Commit changes.
+conn.commit()
+
+# Close groc.db
+conn.close()
+
+
+
